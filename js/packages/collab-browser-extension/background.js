ccfcb7b8bc94daf3fae2f78b4db84f629c24c8c1
Dave Haeffner <dhaeffner@gmail.com>
2022-11-17 10:32:29 -0500

    js/collab-ext: made it so only 1 window is open, and add ability to focus it

--- js/packages/collab-browser-extension/background.js
+++ js/packages/collab-browser-extension/background.js
@@ -1,19 +1,38 @@
+const q = []
+
+// TODO:
+// persist q to local storage
+// cleanup q for windows that no longer exist
+// test coverage
+
 chrome.runtime.onConnect.addListener(function(port) {
   console.assert(port.name === "applitools-collab-open-sesame")
-  port.onMessage.addListener(function(msg) {
+  port.onMessage.addListener(async function(msg) {
     console.log('message received from the content script (via the collab web app)', msg)
-    console.log('opening window')
-    chrome.windows.create(
-      {
-        focused: true,
-        width: msg.size && msg.size.width || 800,
-        height: msg.size && msg.size.height || 600,
-        left: 10,
-        top: 10,
-        type: 'normal',
-        url: msg.url || 'data:text/html,<h1>Hello, World!</h1>',
-      },
-    )
-    console.log('window opened!')
+    const {requiredViewportSize, focusWindow} = msg
+    if (focusWindow) {
+      console.log('bringing open window into focus')
+      chrome.windows.update(focusWindow, {focused: true})
+      return
+    }
+    if (q.length) console.log('there is already a window in the queue, nothing to do', q)
+    if (!q.length) {
+      console.log('opening window')
+      const {id: windowId} = await chrome.windows.create(
+        {
+          focused: true,
+          width: 1024,
+          height: 768,
+          left: 10,
+          top: 10,
+          type: 'normal',
+          url: msg.newWindowUrl || 'data:text/html,<h1>Hello, World!</h1>',
+        },
+      )
+      console.log('window opened!')
+      console.log('adding to the queue', {windowId, requiredViewportSize})
+      q.push({windowId, requiredViewportSize})
+      port.postMessage({windowId})
+    }
   })
-});
+})
--- js/packages/collab-browser-extension/content-script.js
+++ js/packages/collab-browser-extension/content-script.js
@@ -1,4 +1,4 @@
-var port = chrome.runtime.connect({name: 'applitools-collab-open-sesame'});
+var port = chrome.runtime.connect({name: 'applitools-collab-open-sesame'})
 
 const flag = document.createElement('div')
 flag.style.display = 'none'
@@ -8,12 +8,20 @@ document.body.appendChild(flag)
 window.addEventListener('message', (event) => {
   // We only accept messages from ourselves
   if (event.source != window) {
-    return;
+    return
   }
 
   if (event.data.type && (event.data.type == 'FROM_PAGE')) {
-    console.log('Content script received: ' + event.data.text);
+    console.log('Content script received: ' + event.data.text)
     console.log('event data', event.data)
-    port.postMessage(event.data);
+    port.postMessage(event.data)
   }
-}, false);
+}, false)
+
+port.onMessage.addListener(function(msg) {
+  console.log('msg received from background script', msg)
+  window.postMessage({
+    type: 'FROM_EXTENSION',
+    ...msg
+  })
+})
