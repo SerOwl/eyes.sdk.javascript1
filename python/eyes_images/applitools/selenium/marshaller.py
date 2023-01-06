bb556a655ea5b67dd2bfd51636a0f9aec3f37057
Igor Kozyrenko <igor.kozyrenko@applitools.com>
2023-01-05 19:02:18 -0800

    feat: Marshaller passes object registry to all schemas

--- python/eyes_images/applitools/selenium/marshaller.py
+++ python/eyes_images/applitools/selenium/marshaller.py
@@ -34,46 +34,50 @@ class Marshaller(object):
     def __init__(self, object_registry):
         # type: (ObjectRegistry) -> None
         self._object_registry = object_registry
+        self._sa = dict(context={"registry": object_registry})
 
     def marshal_viewport_size(self, viewport_size):
         # type: (ViewPort) -> dict
-        return check_error(Size().dump(viewport_size))
+        return check_error(Size(**self._sa).dump(viewport_size))
 
     def marshal_webdriver_ref(self, driver):
         # type: (WebDriver) -> dict
-        return check_error(StaticDriver().dump(driver))
+        return check_error(StaticDriver(**self._sa).dump(driver))
 
     def marshal_enabled_batch_close(self, close_batches):
         # type: (_EnabledBatchClose) -> dict
-        return check_error(CloseBatchSettings().dump(close_batches))
+        return check_error(CloseBatchSettings(**self._sa).dump(close_batches))
 
     def marshal_delete_test_settings(self, test_results):
         # type: (common.TestResults) -> dict
-        return check_error(DeleteTestSettings().dump(test_results))
+        return check_error(DeleteTestSettings(**self._sa).dump(test_results))
 
     def marshal_configuration(self, configuration):
         # type: (cfg.Configuration) -> dict
-        open = check_error(OpenSettings().dump(configuration))
-        config = check_error(EyesConfig().dump(configuration))
-        close = check_error(CloseSettings().dump(configuration))
+        open = check_error(OpenSettings(**self._sa).dump(configuration))
+        config = check_error(EyesConfig(**self._sa).dump(configuration))
+        close = check_error(CloseSettings(**self._sa).dump(configuration))
         return {"open": open, "screenshot": config, "check": config, "close": close}
 
     def marshal_check_settings(self, check_settings):
         # type: (cs.SeleniumCheckSettings) -> dict
-        return check_error(CheckSettings().dump(check_settings.values))
+        return check_error(CheckSettings(**self._sa).dump(check_settings.values))
 
     def marshal_image_target(self, image_target):
         # type: (t.ImageTarget) -> dict
-        return check_error(ImageTarget().dump(image_target))
+        return check_error(ImageTarget(**self._sa).dump(image_target))
 
     def marshal_locate_settings(self, locate_settings):
         # type: (locators.VisualLocatorSettings) -> dict
-        return check_error(LocateSettings().dump(locate_settings.values))
+        return check_error(LocateSettings(**self._sa).dump(locate_settings.values))
 
     def marshal_ocr_extract_settings(self, extract_settings):
         # type: (Tuple[extract_text.OCRRegion, ...]) -> List[dict]
-        return [check_error(ExtractTextSettings().dump(s)) for s in extract_settings]
+        return [
+            check_error(ExtractTextSettings(**self._sa).dump(s))
+            for s in extract_settings
+        ]
 
     def marshal_ocr_search_settings(self, search_settings):
         # type: (extract_text.TextRegionSettings) -> dict
-        return check_error(OCRSearchSettings().dump(search_settings))
+        return check_error(OCRSearchSettings(**self._sa).dump(search_settings))
